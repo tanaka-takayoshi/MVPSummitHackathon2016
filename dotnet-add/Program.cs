4b7eefe94eff6018c58cbb593e05a65945ab9e6d
tanaka_733 <tanaka733@noreply.github.com>
2016-11-11 08:16:39 +0900

    Added dotnet-add CLI extension.

---
+++ dotnet-add/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //DEBUG
+        Console.WriteLine(string.Join(",", args));
+        Console.WriteLine(Directory.GetCurrentDirectory());
+        //END DEBUG
+        var type = args.Length >= 1 ? args[0] : "cs";
+        var name = args.Length >= 2 ? args[1] : null;
+
+        switch (type)
+        {
+            case "cs":
+                GenerateCsFile(name);
+                break;
+            case "if":
+                GenerateInterfaceFile(name);
+                break;
+            default:
+                break;
+        }
+     }
+
+    private static void GenerateCsFile(string name)
+    {
+        var path = Path.GetExtension(name) != ".cs" ? name + ".cs" : name;
+        path = Path.Combine(Directory.GetCurrentDirectory(), path);
+        if (File.Exists(path))
+        {
+            Console.WriteLine($"{path} is existing. Do nothing.");
+            return;
+        }
+        var dir = Path.GetDirectoryName(path);
+        //Debug
+        Console.WriteLine($"Dir: {dir}");       
+        if (!string.IsNullOrEmpty(dir) && !Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+        using (var writer = File.CreateText(path))
+        {
+            var className = Path.GetFileNameWithoutExtension(path);
+            writer.Write($@"using System;
+using System.IO;
+using System.Linq;
+
+namespace Example
+{{
+    class {className}
+    {{
+
+    }}
+}}");
+        }
+        Console.WriteLine($"Added class to {path}");
+    }
+
+    private static void GenerateInterfaceFile(string name)
+    {
+        var path = Path.GetExtension(name) != ".cs" ? name + ".cs" : name;
+        path = Path.Combine(Directory.GetCurrentDirectory(), path);
+        if (File.Exists(path))
+        {
+            Console.WriteLine($"{path} is existing. Do nothing.");
+            return;
+        }
+        var dir = Path.GetDirectoryName(path);
+        //Debug
+        Console.WriteLine($"Dir: {dir}");
+        if (!string.IsNullOrEmpty(dir) && !Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+        using (var writer = File.CreateText(path))
+        {
+            var className = Path.GetFileNameWithoutExtension(path);
+            writer.Write($@"using System;
+using System.IO;
+using System.Linq;
+
+namespace Example
+{{
+    interface {className}
+    {{
+
+    }}
+}}");
+            Console.WriteLine($"Added interface to {path}");
+        }
+    }
+    
+}
+
--- pack.ps1
+++ pack.ps1
@@ -1,7 +1,19 @@
+cd $PSScriptRoot
 dotnet restore
 dotnet build
-cd dotnet-packer
+rm ./nupkgs/*
+rm $env:USERPROFILE\".nuget\packages\dotnet-add"
+cd ./dotnet-add
 dotnet pack -o ../nupkgs
 cd ../SampleTargets.PackerTarget
 dotnet pack -o ../nupkgs
 cd ..
+cd ./sample/ConsumingProject
+dotnet restore
+dotnet add cs Test1
+dotnet add cs Test/Test2
+dotnet add if ITest
+
+
+
+
